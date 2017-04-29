d1e3a5c4fe453c97dcf2535c587d5398a351a9d8
Bo Huang <iamhuangbo@gmail.com>
2017-04-29 03:25:32 -0500

    unfinished

---
+++ motorAndScreen.c
@@ -0,0 +1,116 @@
+#include <Adafruit_GFX.h>
+#include <gfxfont.h>
+#include <Adafruit_SSD1306.h>
+
+const int OLED_CLK   = D0;
+const int OLED_MOSI  = D1;
+const int OLED_RESET = D2;
+const int OLED_DC    = D3;
+const int OLED_CS    = D4;
+
+Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
+
+// Disable WiFi (for now)
+SYSTEM_MODE(MANUAL);
+
+// 3-4 Letter string to help spot device
+ const char * const deviceName = "FOO";
+
+// Define a service and its data: The "numbers" are randomly generated from https://www.uuidgenerator.net/
+BLEService simpleCustomService("208c9c6f-dcf8-4c1f-8a43-8f1674c21d6e");
+BLECharacteristic fanChangeCharacteristic("a7360086-35eb-405e-8fa9-5060fc4f60e8", ATT_PROPERTY_READ | ATT_PROPERTY_WRITE);
+BLECharacteristic oledDisplayCharacteristic("de356095-f965-4a5f-9418-41a48ea6718d" , ATT_PROPERTY_READ | ATT_PROPERTY_WRITE);
+
+
+// Declare a servo object:
+Servo servo;
+int count =0;
+
+void rotateFan() {
+    // publish the event that will trigger our Webhook
+    if (count % 2 == 0){
+        servo.write(120); 
+        count += 1;
+    }
+    else{
+        servo.write(0); 
+        count += 1;
+    }
+}
+
+Timer motorTimer(360, rotateFan);
+
+void setup() {
+  display.begin(SSD1306_SWITCHCAPVCC);
+  Serial.begin(9600);
+  servo.attach( D0 );
+
+  //Setup for charateristic
+  pinMode(ON_PIN, INPUT_PULLUP);
+  attachInterrupt( digitalPinToInterrupt(ON_PIN), onChanged, CHANGE);
+  pinMode(OFF_PIN, INPUT_PULLUP);
+  attachInterrupt( digitalPinToInterrupt(OFF_PIN), offChanged, CHANGE);
+  Serial.println("Start program");
+  byte fanValue[] = {0};  // A 1 byte integer value for on or off
+  fanChangeCharacteristic.setValue(fanValue,1);
+  fanChangeCharacteristic.setCallback(fanChangeCallback);
+  simpleCustomService.addCharacteristic(fanChangeCharacteristic);
+  byte oledValue[] = {0};  // One byte for integer value and one for decimal
+  oledDisplayCharacteristic.setValue(oledValue,1);
+  oledDisplayCharacteristic.setCallback(oledChangeCallback);
+  simpleCustomService.addCharacteristic(oledDisplayCharacteristic);
+  
+  // Add the Service
+  DuoBLE.addService(simpleCustomService);
+  // Start stack
+  DuoBLE.begin();
+  // The Advertised name and "local" name should have some agreement
+  DuoBLE.advertisingDataAddName(ADVERTISEMENT, deviceName);
+  DuoBLE.setName(deviceName);
+  // Start advertising.
+  DuoBLE.startAdvertising();
+  Serial.println("BLE start advertising.");
+  
+}
+
+void loop() {
+  delay(8000);
+  //changeFanState();
+  printOnScreen (823);
+  delay(8000);
+  //changeFanState();
+  printOnScreen (998);
+}
+
+void printOnScreen(int temperature){
+  if (temperature == -1111){
+    display.clearDisplay();   // clears the screen and buffer 
+  }
+  else{
+  display.clearDisplay();   // clears the screen and buffer  
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  float tempF = temperature/1.0;
+  String stringTemp =  String(tempF, 1);
+  String line = "Temp: " + stringTemp;
+  display.println(line);
+  display.display();
+  display.startscrollright(0,31);
+  }
+}
+
+void changeFanState(int onOff){
+//  if (motorTimer.isActive()){
+//    motorTimer.stop();
+//  }
+//  else{
+//    motorTimer.start();
+//  }
+  if (onOff % 2 == 0){
+    motorTimer.stop();
+  }
+  else{
+    motorTimer.start();
+  }
+}
